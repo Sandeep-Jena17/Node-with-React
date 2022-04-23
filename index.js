753efbba07836552c35447788ddfeba0ff63c9ba
Sandeep <sandeep.jena@empower-solutions.co.in>
2022-04-24 00:38:45 +0530

    updated

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ app.get('/',(req,res)=>{
     res.send( {
         name:"Express"
     })
+
 })
+
+const PORT=process.env.PORT;
 app.listen(8000,()=>{
     console.log('server is running on 8000');
 })
+// Sandeep_@#420
